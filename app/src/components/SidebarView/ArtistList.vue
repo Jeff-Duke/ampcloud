77f0b565462e2538f24e05df861fdca1aba781e9
Ben Godfrey <bcgodfrey91@gmail.com>
2016-12-06 00:02:35 -0700

    Add base sidebar functionality.

--- app/src/components/SidebarView/ArtistList.vue
+++ app/src/components/SidebarView/ArtistList.vue
@@ -58,7 +58,6 @@ export default {
     },
   },
   created() {
-      // Set $route values that are not preset during unit testing
     if (process.env.NODE_ENV === 'testing') {
       this.$route = {
         name: 'home-page',
