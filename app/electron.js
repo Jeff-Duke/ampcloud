c4c96b286b2eb046db57f3c112439cf28313359b
Jeff Duke <jeff.duke@gmail.com>
2016-12-11 13:11:47 -0700

    clean up main-process.  configure file opening button/function for the player

--- app/electron.js
+++ app/electron.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const electron = require('electron');
+const { app, BrowserWindow, dialog} = require('electron');
+const fs = require('fs');
 const path = require('path');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
 
 const gimmeSong = global.gimmeSong = require('./gimme-song-url');
 
@@ -11,11 +10,11 @@ let mainWindow;
 let config = {};
 
 if (process.env.NODE_ENV === 'development') {
-  config = require('../config')
-  config.url = `http://localhost:${config.port}`
+  config = require('../config');
+  config.url = `http://localhost:${config.port}`;
 } else {
-  config.devtron = false
-  config.url = `file://${__dirname}/dist/index.html`
+  config.devtron = false;
+  config.url = `file://${__dirname}/dist/index.html`;
 }
 
 function createWindow () {
@@ -27,42 +26,54 @@ function createWindow () {
     width: 1200,
     webSecurity: false,
     experimentalFeatures: true
-  })
+  });
 
-  mainWindow.loadURL(config.url)
+  mainWindow.loadURL(config.url);
 
   if (process.env.NODE_ENV === 'development') {
-    BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/devtron'))
+    BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/devtron'));
 
-    let installExtension = require('electron-devtools-installer')
+    let installExtension = require('electron-devtools-installer');
 
     installExtension.default(installExtension.VUEJS_DEVTOOLS)
       .then((name) => mainWindow.webContents.openDevTools())
-      .catch((err) => console.log('An error occurred: ', err))
+      .catch((err) => console.log('An error occurred: ', err));
   }
 
   mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-
-  console.log('mainWindow opened')
-}
-
-function playAudio(file) {
-
+    mainWindow = null;
+  });
+  
+  console.log('mainWindow opened');
 }
 
 app.on('ready', createWindow);
 
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
+
+const openFile = exports.openFile = () => {
+  const files = dialog.showOpenDialog({
+    title: 'Open File',
+    properties: [ 'openFile' ],
+    filters: [
+      {name: 'Audio Files', extensions: ['mp3']},
+    ]
+  });
+
+  if (!files) { return; }
+
+  const file = files[0];
+  // const content = fs.readFileSync(files[0]).toString();
+
+  console.log(file);
+};
--- app/src/components/PlayerView/Player.vue
+++ app/src/components/PlayerView/Player.vue
@@ -13,22 +13,29 @@
     <input type="file" id="file" />
     <button @click='nextTrack'>nextTrack</button>
     <button @click='loadTrack'>LoadTrack</button>
+    <button @click='openFile'>OpenFile</button>
 
   </section>
 </template>
 
 <script>
-  export default {
-    methods: {
-      nextTrack() {
-      },
-      prevTrack() {
-      },
-      loadTrack() {
-        require('electron').remote.getGlobal('gimmeSong')((err, song) => {
-          document.getElementById('audio-player').src = song;
-        });
-      },
+import { remote } from 'electron';
+import path from 'path';
+const mainProcess = remote.require(path.join(process.cwd(), 'app/electron.js'));
+export default {
+  methods: {
+    openFile() {
+      mainProcess.openFile();
     },
-  };
+    nextTrack() {
+    },
+    prevTrack() {
+    },
+    loadTrack() {
+      require('electron').remote.getGlobal('gimmeSong')((err, song) => {
+        document.getElementById('audio-player').src = song;
+      });
+    },
+  },
+};
 </script>
